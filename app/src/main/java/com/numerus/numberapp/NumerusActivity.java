ee140340d8be9d713c813c491cb06b3c7718645c
Kiran Kumar <kiran.kumar@cricbuzz.com>
2017-10-10 16:02:57 +0530

    Added Android Basic App

---
+++ app/src/main/java/com/numerus/numberapp/NumerusActivity.java
@@ -0,0 +1,13 @@
+package com.numerus.numberapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NumerusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numerus);
+    }
+}
